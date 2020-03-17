571f40a22090e398e1087431f811f70423b8b855
SpzGen <zvezdochkin@mail.ru>
2020-03-17 20:06:20 +0400

    fix: add function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let res = [];
+  if(Array.isArray(arr)){
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof(arr[i]) === 'string') {
+        let arrCat = arr[i].trim().toUpperCase();
+        for (let j = 0; j < arrCat.length; j++) {
+          if (typeof(arrCat[j] === 'string')) {
+              res = res.concat(arrCat[j]);
+              break;
+          }           
+        }
+      }       
+    }
+  }
+  return res.sort().join('');
 };
