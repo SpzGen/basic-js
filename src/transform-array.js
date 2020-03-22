5d57cc88ac48b7fbafb309aeb7984d0fef2ac879
SpzGen <zvezdochkin@mail.ru>
2020-03-22 19:39:51 +0400

    fix: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let NewArr = [];
+    if(!Array.isArray(arr)) throw new Error ('Error');
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next' : {
+                if (i < arr.length-1) i++;
+                break;
+            }
+            case '--discard-prev' : {
+                if (i > 0) NewArr.pop();
+                break;
+            }           
+            case '--double-next' : {
+                if(i < arr.length-1) NewArr.push(arr[i+1]);
+                break;
+            }
+            case '--double-prev' : {
+                if(i > 0) NewArr.push(arr[i-1]);
+                break;
+            }
+            default: NewArr.push(arr[i]);
+        }
+    }
+return NewArr;
 };
